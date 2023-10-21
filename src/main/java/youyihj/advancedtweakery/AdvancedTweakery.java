8dd0f52c43a312c81c1962ab4c618de6f6b829a2
youyihj <youyi580@qq.com>
2023-10-21 10:59:05 +0800

    v1.2

--- src/main/java/youyihj/advancedtweakery/AdvancedTweakery.java
+++ src/main/java/youyihj/advancedtweakery/AdvancedTweakery.java
@@ -22,7 +22,7 @@ public class AdvancedTweakery {
 
     public static final String MOD_ID = "advancedtweakery";
     public static final String MOD_NAME = "AdvancedTweakery";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
     public static final String DEPENDENCIES = "required-after:crafttweaker;required-after:advancedrocketry";
 
     /**
